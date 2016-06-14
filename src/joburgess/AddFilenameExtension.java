2fdb0fef660bd91fca9a76e1c4c80134f46606e9
Joburgess <Joburgess@Joburgess.Joburgess>
2016-06-14 20:18:22 +0800

    Joburgess

---
+++ src/joburgess/AddFilenameExtension.java
@@ -0,0 +1,30 @@
+package joburgess;
+
+import java.io.File;
+
+public class AddFilenameExtension {
+
+	public boolean changeFilenameExtension(String oldPath,String newPath){
+		File oldDirect=new File(oldPath);
+		
+		if(oldDirect!=null&&oldDirect.isDirectory()){
+			File[] files=oldDirect.listFiles();
+			for (File file : files) {
+				if(file!=null&&file.isFile()){
+					System.out.println(file.getAbsolutePath());
+					file.renameTo(new File(file.getAbsolutePath()+".jpg"));
+				}
+			}
+		}else{
+			System.out.println("需要修改的文件所在的目录有问题！");
+		}
+		
+		return false;
+	}
+
+	public static void main(String[] args) {
+		AddFilenameExtension e=new AddFilenameExtension();
+		e.changeFilenameExtension("G:/LOST.DIR/", "G:/");
+	}
+	
+}
